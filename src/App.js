71c5312d42bb8caace42909ef65f5c7c30eefbb9
Fatima <fatimaolasunkanmi01@gmail.com>
2021-01-07 21:48:25 +0100

    background colour added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Landing from './components/Landing/index';
 
-function App() {
+const App = () => {
   return (
     <div>
-     Aquila
+      <Landing />
     </div>
   );
 }
---
+++ src/components/Landing/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'materialize-css';
+
+const Index = () => {
+    return (
+        <div className="layer">
+            <div className="box-image">
+                <img className="responsive-img" src="aquila.PNG" alt="aquila girl" width="700px" />
+            </div>
+        </div>
+    )
+}
+
+export default Index
